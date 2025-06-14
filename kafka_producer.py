b8158ce6d83aa912c4357d933adfde4fd92dd998
afunes99uoc <afunes99@uoc.edu>
2025-06-14 19:19:34 +0200

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,1 +1,32 @@
+from kafka import KafkaProducer
+import json
+import time
+import requests
+
+# Configuración del productor
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# URL de Open-Meteo (Barcelona)
+url = 'https://api.open-meteo.com/v1/forecast?latitude=41.38&longitude=2.17&hourly=temperature_2m,relative_humidity_2m'
+
+# Simulación de datos cada 5 segundos
+while True:
+    response = requests.get(url)
+    data = response.json()
+    horas = data["hourly"]["time"]
+    temperaturas = data["hourly"]["temperature_2m"]
+    humedades = data["hourly"]["relative_humidity_2m"]
+
+    for i in range(len(horas)):
+        mensaje = {
+            "hora": horas[i],
+            "temperatura": temperaturas[i],
+            "humedad": humedades[i]
+        }
+        print("Enviando:", mensaje)
+        producer.send('clima-barcelona', value=mensaje)
+        time.sleep(1)
 
