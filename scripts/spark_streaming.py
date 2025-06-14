2a9966a50779070c3e2df25276752259d7d7c1ee
afunes99uoc <afunes99@uoc.edu>
2025-06-14 22:23:29 +0200

    Update spark_streaming.py

--- scripts/spark_streaming.py
+++ scripts/spark_streaming.py
@@ -1,12 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, avg
+from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, FloatType
 import os
 
 # Configuración de entorno Hadoop
 os.environ["HADOOP_HOME"] = "C:/hadoop"
 
-# Crear sesión de Spark
 spark = SparkSession.builder \
     .appName("StreamingClimaBarcelona") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
@@ -14,7 +13,6 @@ spark = SparkSession.builder \
 
 spark.sparkContext.setLogLevel("WARN")
 
-# Definir esquema de los datos del mensaje
 schema = StructType() \
     .add("ciudad", StringType()) \
     .add("temperatura_2m", FloatType()) \
@@ -23,7 +21,6 @@ schema = StructType() \
     .add("precipitation", FloatType()) \
     .add("surface_pressure", FloatType())
 
-# Leer de Kafka
 df_kafka = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
@@ -31,23 +28,12 @@ df_kafka = spark.readStream \
     .option("startingOffsets", "latest") \
     .load()
 
-# Parsear JSON
 df_stream = df_kafka.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
-# Cálculo de KPIs por barrio (agrupación)
-kpis = df_stream.groupBy("ciudad").agg(
-    avg("temperatura_2m").alias("temp_media"),
-    avg("wind_speed_10m").alias("viento_medio"),
-    avg("relative_humidity_2m").alias("humedad_media"),
-    avg("precipitation").alias("precipitacion_media"),
-    avg("surface_pressure").alias("presion_media")
-)
-
-# Escritura en carpeta oro
-query = kpis.writeStream \
-    .outputMode("complete") \
+query = df_stream.writeStream \
+    .outputMode("append") \
     .format("parquet") \
     .option("checkpointLocation", "./checkpoints/clima") \
     .option("path", "./data/oro/clima") \
